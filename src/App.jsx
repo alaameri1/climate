84493d926d6e49ba179ecbf3d8a0b608243028a1
baha ouni <bahaouni1@gmail.com>
2023-12-07 23:55:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Navbar';
+import Web from './Web';
+
+function App() {
+  return (
+    <div className="App">
+     <Web />
+    </div>
+  );
+}
+
+export default App;
